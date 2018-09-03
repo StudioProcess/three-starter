6bf79c525accf7996393fd9283a32a1e44360d17
Martin Leopold <m@martinleopold.com>
2018-09-03 16:24:25 +0200

    Better browser support for toggleFullscreen utility function

--- app/util.js
+++ app/util.js
@@ -1,7 +1,17 @@
 export function toggleFullscreen() {
-  if (!document.webkitFullscreenElement) {
-    document.querySelector('body').webkitRequestFullscreen();
-  } else { document.webkitExitFullscreen(); }
+  if (document.webkitFullscreenEnabled) { // Chrome, Opera, Safari
+    if (!document.webkitFullscreenElement) {
+      document.querySelector('body').webkitRequestFullscreen();
+    } else { document.webkitExitFullscreen(); }
+  } else if (document.mozFullScreenEnabled) { // Firefox
+    if (!document.mozFullScreenElement) {
+      document.querySelector('body').mozRequestFullScreen();
+    } else { document.mozCancelFullScreen(); }
+  } else if (document.fullscreenEnabled) { // Standard, Edge
+    if (!document.fullscreenElement) {
+      document.querySelector('body').requestFullscreen();
+    } else { document.exitFullscreen(); }
+  }
 }
 
 
