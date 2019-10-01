27df818c82016e22a7325248da7e6466f0229ff3
Martin Leopold <m@martinleopold.com>
2019-10-01 16:05:54 +0200

    Updated saveCanvas() for Firefox

--- app/util.js
+++ app/util.js
@@ -16,11 +16,15 @@ export function toggleFullscreen() {
 
 
 // NOTE: Needs THREE.WebGLRenderer with preserveDrawingBuffer=true
+// TODO: Firefox seems to save only the bottom left quadrant of the canvas. This also happens with 'Right-Click/Save Image as...'
 export function saveCanvas() {
   let canvas = document.querySelector('canvas');
   let link = document.createElement('a');
   let timestamp = new Date().toISOString();
   link.download = timestamp + '.png';
   link.href = canvas.toDataURL();
+  link.style.display = 'none';     // Firefox
+  document.body.appendChild(link); // Firefox
   link.click();
+  document.body.removeChild(link); // Firefox
 }
